90366c19730a8f067007360b56588d3a4b43e46f
cheeou <best.coder.jiwoo@gmail.com>
2024-09-25 02:53:32 +0900

    ✨ Update 9/24 chal

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,12 +1,32 @@
 from django.contrib import admin
 from .models import Tweet, Like
 
+class ElonMuskFilter(admin.SimpleListFilter):
+    title = 'Elon Musk 포함 여부'  # 필터의 제목
+    parameter_name = 'elon_musk'  # URL에 사용될 파라미터 이름
 
+    def lookups(self, request, model_admin):
+        """ 필터에서 제공할 선택지 """
+        return [
+            ('yes', 'Elon Musk 포함'),
+            ('no', 'Elon Musk 미포함'),
+        ]
+
+    def queryset(self, request, queryset):
+        """ 필터가 적용되었을 때 실행될 쿼리셋 필터링 로직 """
+        if self.value() == 'yes':
+            return queryset.filter(payload__icontains='Elon Musk')  # 'Elon Musk'가 포함된 트윗 필터링
+        if self.value() == 'no':
+            return queryset.exclude(payload__icontains='Elon Musk')  # 'Elon Musk'가 포함되지 않은 트윗 필터링
+        return queryset
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
     list_display = ('payload', 'user', 'like_count')
-    
-    def like_count(self, obj):
-        return obj.likes.count()
+    list_filter = [ElonMuskFilter]
 
 
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('tweet', 'user', 'created_at')  
+    search_fields = ('user__username',) 
+    list_filter = ('created_at',)  
--- tweets/models.py
+++ tweets/models.py
@@ -10,11 +10,14 @@ class Tweet(CommonModel):
 
     def __str__(self):
         return self.payload
+    def like_count(self):
+        return self.likes.count()
+
 
 class Like(CommonModel):
     """ Like Model Definition """
 
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='likes')
     tweet = models.ForeignKey('tweets.Tweet', on_delete=models.CASCADE, related_name='likes')
 
     def __str__(self):
